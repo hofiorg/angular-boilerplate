53b0c66e2add4f19e5d7a6cb69838f76ab64110a
hofiorg <hofiorg@googlemail.com>
2024-11-08 12:50:43 +0100

    converted test using WebTestClient

--- src/test/java/DemoRestControllerTest.java
+++ src/test/java/DemoRestControllerTest.java
@@ -3,80 +3,71 @@ import org.hofi.DemoService;
 import org.hofi.model.Demo;
 import org.hofi.model.DemoFilter;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import static org.mockito.Mockito.*;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.Instant;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DemoRestControllerTest {
 
   @Autowired
-  private TestRestTemplate restTemplate;
+  private WebTestClient webTestClient;
 
   @MockBean
   private DemoService demoService;
 
   @Test
   public void testSearchDemos_Success() {
-    DemoFilter demoFilter = new DemoFilter(Instant.now(), Instant.now(), "", "", "", "");
+    DemoFilter demoFilter = new DemoFilter(
+      Instant.parse("2024-01-01T00:00:00Z"),
+      Instant.parse("2024-12-31T23:59:59Z"),
+      "", "", "", "");
 
-    List<Demo> result = new ArrayList<>();
-    result.add(new Demo("Name", "Gender", "Company"));
+    List<Demo> result = List.of(new Demo("Name", "Gender", "Company"));
 
-    when(demoService.search(any())).thenReturn(result);
+    when(demoService.search(any(DemoFilter.class))).thenReturn(result);
 
-    HttpEntity<DemoFilter> requestEntity = new HttpEntity<>(demoFilter);
-    ResponseEntity<String> responseEntity = restTemplate.exchange(
-      "/api/demo/search",
-      HttpMethod.POST,
-      requestEntity,
-      String.class
-    );
+    webTestClient.post()
+      .uri("/api/demo/search")
+      .body(Mono.just(demoFilter), DemoFilter.class)
+      .exchange()
+      .expectStatus().isOk()
+      .expectBody()
+      .json("""
+                  [{"name":"Name","gender":"Gender","company":"Company"}]""");
 
-    assertEquals(200, responseEntity.getStatusCode().value());
-    assertEquals("""
-    [{"name":"Name","gender":"Gender","company":"Company"}]""", responseEntity.getBody());
-
-    verify(demoService, times(1)).search(any());
+    Mockito.verify(demoService, times(1)).search(any(DemoFilter.class));
   }
 
   @Test
   public void testSearchDemos_BadRequest_EingangVon_Null() {
     DemoFilter demoFilter = new DemoFilter(null, null, "", "", "", "");
-    HttpEntity<DemoFilter> requestEntity = new HttpEntity<>(demoFilter);
 
-    ResponseEntity<String> responseEntity = restTemplate.exchange(
-      "/api/demo/search",
-      HttpMethod.POST,
-      requestEntity,
-      String.class
-    );
-
-    assertEquals(400, responseEntity.getStatusCode().value());
+    webTestClient.post()
+      .uri("/api/demo/search")
+      .body(Mono.just(demoFilter), DemoFilter.class)
+      .exchange()
+      .expectStatus().isBadRequest();
   }
 
   @Test
   public void testSearchDemos_BadRequest_System_Length31() {
     DemoFilter demoFilter = new DemoFilter(Instant.now(), Instant.now(), "0123456789012345678901234567891", "", "", "");
-    HttpEntity<DemoFilter> requestEntity = new HttpEntity<>(demoFilter);
 
-    ResponseEntity<String> responseEntity = restTemplate.exchange(
-      "/api/demo/search",
-      HttpMethod.POST,
-      requestEntity,
-      String.class
-    );
-
-    assertEquals(400, responseEntity.getStatusCode().value());
+    webTestClient.post()
+      .uri("/api/demo/search")
+      .body(Mono.just(demoFilter), DemoFilter.class)
+      .exchange()
+      .expectStatus().isBadRequest();
   }
 }
