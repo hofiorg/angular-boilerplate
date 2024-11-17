5cb5fdceec4ea84f66dc58d31fb7ded57d673e6b
Christian Hofmann <hofiorg@googlemail.com>
2024-11-17 12:27:29 +0100

    better footnote styles

--- src/main/js/app/component/footer/footnote.component.ts
+++ src/main/js/app/component/footer/footnote.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Meta} from '@angular/platform-browser';
 
 @Component({
@@ -6,20 +6,34 @@ import {Meta} from '@angular/platform-browser';
     standalone: true,
     providers: [],
     imports: [],
-    templateUrl: './footnote.component.html'
+    templateUrl: './footnote.component.html',
+    styleUrl: './footnote.component.css'
   })
-  export class FootnoteComponent {
-  
-    version: string | undefined;
-    timestamp: string | undefined;
-    customer: string | undefined;
-  
-    constructor(private meta: Meta) {
-      this.version = this.meta.getTag('name=version')?.content;
-      this.version = this.version === '@project.version@' ? 'DEV-SERVER' : this.version;
-      this.timestamp = this.meta.getTag('name=timestamp')?.content;
-      this.timestamp = this.timestamp === '@timestamp@' ? '-' : this.timestamp;
-      this.customer = this.meta.getTag('name=customer')?.content;
-      this.customer = this.customer === '@customer@' ? '-' : this.customer;
+export class FootnoteComponent implements OnInit {
+  private static hasLogged = false;
+
+  version: string | undefined;
+  timestamp: string | undefined;
+  customer: string | undefined;
+  currentYear = new Date().getFullYear();
+  companyName = 'hofi.org'
+
+  constructor(private meta: Meta) {
+  }
+
+  ngOnInit(): void {
+    this.version = this.meta.getTag('name=version')?.content;
+    this.version = this.version === '@project.version@' ? 'DEV-SERVER' : this.version;
+    this.timestamp = this.meta.getTag('name=timestamp')?.content;
+    this.timestamp = this.timestamp === '@timestamp@' ? 'DEV-SERVER' : this.timestamp;
+    this.customer = this.meta.getTag('name=customer')?.content;
+    this.customer = this.customer === '@customer@' ? 'DEV-SERVER' : this.customer;
+
+    if (!FootnoteComponent.hasLogged && this.version !== undefined) {
+      console.info(
+        `© ${this.currentYear} ⋅ ${this.companyName} ⋅ Build-version: ${this.version} ⋅ Build-timestamp: ${this.timestamp} ⋅ Customer: ${this.customer}`
+      );
+      FootnoteComponent.hasLogged = true; // Set the flag to true after logging
     }
+  }
 }
